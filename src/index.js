49d9dae9675329729f8c1b52e31599e3d5ecfacf
Andreas <a.sutt@khk.ee>
2021-01-09 13:41:03 +0200

    Solution to 1.5

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course} </h1>
+      <h1>{props.course.name} </h1>
     </div>
   )
 }
@@ -13,6 +13,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
       <p>{props.part.name} {props.part.exercises} </p>
+      
   )
 }
 
@@ -38,28 +39,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content  parts={course.parts} />
+      <Total parts={course.parts}  />
     </div>
   )
 }
